32f8fd1eca95fff2442b2365db997dbcd253d6d6
DoneWithWork <fingfingsavage@gmail.com>
2024-11-29 12:22:14 +0800

    updated packages and added GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import CodeIcon from "@/public/images/code-icon.png";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
           {children}
         </ThemeProvider>
       </body>
+      <GoogleAnalytics gaId="G-LMYN8TFT6W" />
     </html>
   );
 }
